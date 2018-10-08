48dc5083dfa3f18e2585a77eef0fe42c47b422ca
Daniel Baluta <daniel.baluta@nxp.com>
2018-10-08 17:30:50 +0300

    MMFMWK-8046-4: dsp_framework: plugins: Add EDMA driver
    Reviewed-by: Cosmin-Gabriel Samoila <cosmin.samoila@nxp.com>
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

---
+++ dsp_framework/plugins/renderer/edma.c
@@ -0,0 +1,172 @@
+/*
+ * edma.c - EDMA driver
+ */
+#include "edma.h"
+
+void edma_fill_tcd(struct nxp_edma_hw_tcd *tcd, u32 src_addr, u32 dst_addr,
+		   u16 attr, u16 soff, u32 nbytes, u32 slast, u16 citer,
+		   u16 biter, u16 doff, u32 dlast_sga, u32 major_int,
+		   u32 disable_req, u32 enable_sg, u32 is_rx) {
+	u16 csr = 0;
+
+	tcd->saddr = src_addr;
+	tcd->daddr = dst_addr;
+
+	tcd->attr = attr;
+	tcd->soff = soff;
+
+	tcd->nbytes = nbytes;
+	tcd->slast  = slast;
+
+	tcd->citer  = citer;
+	tcd->doff   = doff;
+
+	tcd->dlast_sga = dlast_sga;
+	tcd->biter     = biter;
+
+	if (major_int)
+		csr |= EDMA_TCD_CSR_INT_MAJOR;
+
+	if (disable_req)
+		csr |= EDMA_TCD_CSR_D_REQ;
+
+	if (enable_sg)
+		csr |= EDMA_TCD_CSR_E_SG;
+
+	if (is_rx)
+		csr |= EDMA_TCD_CSR_ACTIVE;
+
+	tcd->csr = csr;
+}
+
+void edma_set_tcd(volatile void * edma_addr, struct nxp_edma_hw_tcd *tcd) {
+
+	write32(edma_addr + EDMA_TCD_CSR,  0);
+	write32(edma_addr + EDMA_TCD_SADDR, tcd->saddr);
+	write32(edma_addr + EDMA_TCD_DADDR, tcd->daddr);
+	write16(edma_addr + EDMA_TCD_ATTR, tcd->attr);
+	write16(edma_addr + EDMA_TCD_SOFF, tcd->soff);
+	write32(edma_addr + EDMA_TCD_NBYTES, tcd->nbytes);
+	write32(edma_addr + EDMA_TCD_SLAST, tcd->slast);
+	write16(edma_addr + EDMA_TCD_CITER, tcd->citer);
+	write16(edma_addr + EDMA_TCD_BITER, tcd->biter);
+	write16(edma_addr + EDMA_TCD_DOFF, tcd->doff);
+
+	write32(edma_addr + EDMA_TCD_DLAST_SGA, tcd->dlast_sga);
+	
+	if ((EDMA_TCD_CSR_E_SG | tcd->csr) &&
+		(EDMA_CH_CSR_DONE | read32(edma_addr + EDMA_CH_CSR)))
+		write32(edma_addr + EDMA_CH_CSR, EDMA_CH_CSR_DONE);
+
+	write16(edma_addr + EDMA_TCD_CSR, tcd->csr);
+}
+
+
+void edma_init(volatile void * edma_addr, u32 type,
+	       struct nxp_edma_hw_tcd *tcd,
+	       volatile void * sai_addr,
+	       volatile void * dma_buf_addr,
+	       u32 period_size, u32 period_count) {
+	u32 dma_addr = (u32)dma_buf_addr;
+	u32 period_len = period_size;
+	u32 buf_len = period_len * period_count;
+	u32 dma_addr_next = dma_addr;
+	u32 src_addr, dst_addr;
+	u32 soff, doff, nbytes, attr, iter, last_sg;
+	u32 burst=4, addr_width=2;
+	int i;
+
+	for ( i = 0; i < period_count; i ++ ) {
+		if (dma_addr_next >= dma_addr + buf_len)
+			dma_addr_next = dma_addr;
+		if (type == DMA_MEM_TO_DEV) {
+			src_addr = dma_addr_next;
+			dst_addr = (u32)sai_addr;
+			soff = 2;
+			doff = 0;
+		} else if (type == DMA_DEV_TO_MEM) {
+			src_addr = (u32)sai_addr;
+			dst_addr = dma_addr_next;
+			soff = 0;
+			doff = 2;
+		}
+
+		nbytes = burst * addr_width;
+		attr = (EDMA_TCD_ATTR_SSIZE_16BIT | EDMA_TCD_ATTR_DSIZE_16BIT);
+		iter = period_len / nbytes;
+
+		last_sg = (u32)&tcd[(i+1)%period_count];
+
+		edma_fill_tcd(&tcd[i], src_addr, dst_addr, attr, soff, nbytes, 0,
+					iter, iter, doff, last_sg, 1, 0, 1, 0);
+		dma_addr_next = dma_addr_next + period_len;
+	}
+}
+
+void edma_irq_handler(volatile void * edma_addr) {
+
+	unsigned int intr;
+
+	intr = read32(edma_addr + EDMA_CH_INT);
+	if (!intr)
+		return ;
+	write32(edma_addr + EDMA_CH_INT, 1);
+	/*one buffer finished*/
+
+}
+
+void edma_start(volatile void * edma_addr, struct nxp_edma_hw_tcd * tcd,
+		u32 is_rx) {
+	u32 val;
+
+	edma_set_tcd(edma_addr, &tcd[0]);
+
+	val = read32(edma_addr + EDMA_CH_SBR);
+
+	if (is_rx)
+		val |= EDMA_CH_SBR_RD;
+	else
+		val |= EDMA_CH_SBR_WR;
+
+	write32(edma_addr + EDMA_CH_SBR, val);
+
+	val = read32(edma_addr + EDMA_CH_CSR);
+	val |= EDMA_CH_CSR_ERQ;
+	write32(edma_addr + EDMA_CH_CSR, val);
+
+}
+
+void edma_stop(volatile void * edma_addr) {
+	u32 val;
+	
+	val = read32(edma_addr+ EDMA_CH_CSR);
+	val |= ~EDMA_CH_CSR_ERQ;
+	write32(edma_addr + EDMA_CH_CSR, val);
+}
+
+void edma_dump(volatile void * edma_addr) {
+	LOG1("EDMA_CH_CSR %x\n",  read32(edma_addr + EDMA_CH_CSR));
+	LOG1("EDMA_CH_ES %x\n",   read32(edma_addr + EDMA_CH_ES));
+	LOG1("EDMA_CH_INT %x\n",  read32(edma_addr + EDMA_CH_INT));
+	LOG1("EDMA_CH_SBR %x\n",  read32(edma_addr + EDMA_CH_SBR));
+	LOG1("EDMA_CH_PRI %x\n",  read32(edma_addr + EDMA_CH_PRI));
+
+
+	LOG1("EDMA_TCD_SADDR %x\n",  read32(edma_addr + EDMA_TCD_SADDR));
+	LOG1("EDMA_TCD_SOFF %x\n",   read16(edma_addr + EDMA_TCD_SOFF));
+	LOG1("EDMA_TCD_ATTR %x\n",   read16(edma_addr + EDMA_TCD_ATTR));
+	LOG1("EDMA_TCD_NBYTES %x\n", read32(edma_addr + EDMA_TCD_NBYTES));
+	LOG1("EDMA_TCD_SLAST %x\n",  read32(edma_addr + EDMA_TCD_SLAST));
+
+	LOG1("EDMA_TCD_DADDR %x\n", read32(edma_addr + EDMA_TCD_DADDR));
+	LOG1("EDMA_TCD_DOFF %x\n",  read16(edma_addr + EDMA_TCD_DOFF));
+
+	LOG1("EDMA_TCD_CITER %x\n",     read16(edma_addr + EDMA_TCD_CITER));
+	LOG1("EDMA_TCD_DLAST_SGA %x\n", read32(edma_addr + EDMA_TCD_DLAST_SGA));
+
+	LOG1("EDMA_TCD_CSR %x\n",   read16(edma_addr + EDMA_TCD_CSR));
+	LOG1("EDMA_TCD_BITER %x\n", read16(edma_addr + EDMA_TCD_BITER));
+
+	return;
+}
+
---
+++ dsp_framework/plugins/renderer/edma.h
@@ -0,0 +1,119 @@
+#ifndef _EDMA_H
+#define _EDMA_H
+
+#include "mydefs.h"
+
+#define BIT(x) (1 << x)
+
+#define EDMA_CH_CSR                     0x00
+#define EDMA_CH_ES                      0x04
+#define EDMA_CH_INT                     0x08
+#define EDMA_CH_SBR                     0x0C
+#define EDMA_CH_PRI                     0x10
+#define EDMA_TCD_SADDR                  0x20
+#define EDMA_TCD_SOFF                   0x24
+#define EDMA_TCD_ATTR                   0x26
+#define EDMA_TCD_NBYTES                 0x28
+#define EDMA_TCD_SLAST                  0x2C
+#define EDMA_TCD_DADDR                  0x30
+#define EDMA_TCD_DOFF                   0x34
+#define EDMA_TCD_CITER_ELINK            0x36
+#define EDMA_TCD_CITER                  0x36
+#define EDMA_TCD_DLAST_SGA              0x38
+#define EDMA_TCD_CSR                    0x3C
+#define EDMA_TCD_BITER_ELINK            0x3E
+#define EDMA_TCD_BITER                  0x3E
+
+#define EDMA_CH_SBR_RD                  BIT(22)
+#define EDMA_CH_SBR_WR                  BIT(21)
+#define EDMA_CH_CSR_ERQ                 BIT(0)
+#define EDMA_CH_CSR_EARQ                BIT(1)
+#define EDMA_CH_CSR_EEI                 BIT(2)
+#define EDMA_CH_CSR_DONE                BIT(30)
+#define EDMA_CH_CSR_ACTIVE              BIT(31)
+
+#define EDMA_TCD_ATTR_DSIZE(x)          (((x) & 0x0007))
+#define EDMA_TCD_ATTR_DMOD(x)           (((x) & 0x001F) << 3)
+#define EDMA_TCD_ATTR_SSIZE(x)          (((x) & 0x0007) << 8)
+#define EDMA_TCD_ATTR_SMOD(x)           (((x) & 0x001F) << 11)
+#define EDMA_TCD_ATTR_SSIZE_8BIT        (0x0000)
+#define EDMA_TCD_ATTR_SSIZE_16BIT       (0x0100)
+#define EDMA_TCD_ATTR_SSIZE_32BIT       (0x0200)
+#define EDMA_TCD_ATTR_SSIZE_64BIT       (0x0300)
+#define EDMA_TCD_ATTR_SSIZE_16BYTE      (0x0400)
+#define EDMA_TCD_ATTR_SSIZE_32BYTE      (0x0500)
+#define EDMA_TCD_ATTR_SSIZE_64BYTE      (0x0600)
+#define EDMA_TCD_ATTR_DSIZE_8BIT        (0x0000)
+#define EDMA_TCD_ATTR_DSIZE_16BIT       (0x0001)
+#define EDMA_TCD_ATTR_DSIZE_32BIT       (0x0002)
+#define EDMA_TCD_ATTR_DSIZE_64BIT       (0x0003)
+#define EDMA_TCD_ATTR_DSIZE_16BYTE      (0x0004)
+#define EDMA_TCD_ATTR_DSIZE_32BYTE      (0x0005)
+#define EDMA_TCD_ATTR_DSIZE_64BYTE      (0x0006)
+
+#define EDMA_TCD_SOFF_SOFF(x)           (x)
+#define EDMA_TCD_NBYTES_NBYTES(x)       (x)
+#define EDMA_TCD_NBYTES_MLOFF(x)        (x << 10)
+#define EDMA_TCD_NBYTES_DMLOE           (1 << 30)
+#define EDMA_TCD_NBYTES_SMLOE           (1 << 31)
+#define EDMA_TCD_SLAST_SLAST(x)         (x)
+#define EDMA_TCD_DADDR_DADDR(x)         (x)
+#define EDMA_TCD_CITER_CITER(x)         ((x) & 0x7FFF)
+#define EDMA_TCD_DOFF_DOFF(x)           (x)
+#define EDMA_TCD_DLAST_SGA_DLAST_SGA(x) (x)
+#define EDMA_TCD_BITER_BITER(x)         ((x) & 0x7FFF)
+
+#define EDMA_TCD_CSR_START              BIT(0)
+#define EDMA_TCD_CSR_INT_MAJOR          BIT(1)
+#define EDMA_TCD_CSR_INT_HALF           BIT(2)
+#define EDMA_TCD_CSR_D_REQ              BIT(3)
+#define EDMA_TCD_CSR_E_SG               BIT(4)
+#define EDMA_TCD_CSR_E_LINK             BIT(5)
+#define EDMA_TCD_CSR_ACTIVE             BIT(6)
+#define EDMA_TCD_CSR_DONE               BIT(7)
+
+#define DMA_MEM_TO_DEV 0
+#define DMA_DEV_TO_MEM 1
+
+
+#define IRQSTR_ADDR           0x51080000
+
+#define SAI_ADDR              0x59040000
+#define EDMA_ADDR_SAI_TX      0x592d0000
+#define EDMA_ADDR_SAI_RX      0x592c0000
+
+#define ESAI_ADDR             0x59010000
+#define EDMA_ADDR_ESAI_TX     0x59270000
+#define EDMA_ADDR_ESAI_RX     0x59260000
+
+
+struct nxp_edma_hw_tcd {
+	u32  saddr;
+	u16  soff;
+	u16  attr;
+	u32  nbytes;
+	u32  slast;
+	u32  daddr;
+	u16  doff;
+	u16  citer;
+	u32  dlast_sga;
+	u16  csr;
+	u16  biter;
+}__attribute__ ((aligned(32)));
+
+void edma_init(volatile void * edma_addr,
+		u32 type,
+		struct nxp_edma_hw_tcd *tcd,
+		volatile void * sai_addr,
+		volatile void * dma_buf_addr,
+		u32 period_size,
+		u32 period_count);
+
+void edma_start(volatile void * edma_addr,
+		struct nxp_edma_hw_tcd * tcd,
+		u32 is_rx);
+
+void edma_stop(volatile void * edma_addr);
+void edma_dump(volatile void * edma_addr);
+
+#endif
