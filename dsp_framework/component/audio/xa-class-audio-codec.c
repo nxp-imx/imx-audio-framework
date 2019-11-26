5a0ab6a6064a3d04cfe14874cf4d0b04ff128595
Zhang Peng <peng_zhang_8@nxp.com>
2019-11-26 18:46:58 +0800

    MMFMWK-8692 component: Fix cplay can't exit when play stream syz.mp3
    The issue is cplay can't exit when playing stream syz.mp3, the reason is
    that dsp crashed in function xf_input_port_complete, it returned null
    msg, thus changes its judgement condition.
    
    Signed-off-by: Zhang Peng <peng_zhang_8@nxp.com>

--- dsp_framework/component/audio/xa-class-audio-codec.c
+++ dsp_framework/component/audio/xa-class-audio-codec.c
@@ -589,13 +589,10 @@ static UA_ERROR_TYPE xa_codec_postprocess(struct XACodecBase *base, u32 ret)
 		}
 
 	}
-	if (!xf_input_port_done(&codec->input) &&(ret == ACODEC_NOT_ENOUGH_DATA || !codec->input.filled)) {
-		if (!codec->input.remaining) {
-			xf_input_port_complete(&codec->input);
-
+	if (!xf_input_port_done(&codec->input) && xf_input_port_ready(&codec->input) && (ret == ACODEC_NOT_ENOUGH_DATA || !codec->input.filled) && !codec->input.remaining) {
+		xf_input_port_complete(&codec->input);
 		/* ...clear input-setup flag */
 		base->state ^= XA_CODEC_FLAG_INPUT_SETUP;
-		}
 	}
 
 	/* ...output buffer maintenance; check if we have produced anything */
