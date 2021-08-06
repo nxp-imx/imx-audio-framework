8d94a5ab73ebbfcc16ed71b5ddaefae4d901ae9d
Shengjiu Wang <shengjiu.wang@nxp.com>
2021-08-06 20:11:44 +0800

    MMFMWK-8983-3: Revert "MMFMWK-8979-3: common: use rpmsg device for msg channel"
    This reverts commit 6b71129ff2f2ff85b98ff8e135673a28f17d6c41.
    
    As in /dev/dma_heap/dsp we use uncached mmap operation, so
    this patch can be revert
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>

--- common/api/xaf-api.c
+++ common/api/xaf-api.c
@@ -105,9 +105,10 @@ int xaf_comp_set_config(struct xaf_comp *p_comp, u32 num_param, void *p_param)
 	/* ...set persistent stream characteristics */
 	smsg = xf_buffer_data(p_handle->aux);
 
-	/* Fixme: meet alignment issue for memcpy*/
-	for (i = 0; i < num_param; i++)
-		smsg[i] = param[i];
+	for (i = 0; i < num_param; i++) {
+		smsg[i].id = param[i].id;
+		smsg[i].mixData = param[i].mixData;
+	}
 
 	/* ...pass command to the component */
 	ret = xf_command(p_handle,
@@ -177,9 +178,8 @@ int xaf_comp_get_config(struct xaf_comp *p_comp, u32 num_param, void *p_param)
 		return -EPIPE;
 	}
 
-	/* Fixme: meet alignment issue for memcpy*/
 	for (i = 0; i < num_param; i++)
-		param[i] = smsg[i];
+		param[i].mixData = smsg[i].mixData;
 
 	return 0;
 }
