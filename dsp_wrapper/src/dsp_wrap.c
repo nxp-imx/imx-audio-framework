451b8740b3bc653c0b5dd17996987d8349bcb5e1
Zhang Peng <peng.zhang_8@nxp.com>
2022-05-06 17:00:31 +0800

    MMFMWK-9099 dsp_wrapper: Add channel num check for aac codec
    AAC codec can't supprot multi channls, need return not support when
    channls num is over 2.
    
    Signed-off-by: Zhang Peng <peng.zhang_8@nxp.com>

--- dsp_wrapper/src/dsp_wrap.c
+++ dsp_wrapper/src/dsp_wrap.c
@@ -481,6 +481,9 @@ UA_ERROR_TYPE DSPDecSetPara(UniACodec_Handle pua_handle,
 	case UNIA_CHANNEL:
 		if (parameter->channels == 0 || parameter->channels > 8)
 			return ACODEC_PARA_ERROR;
+		/* ... cadence aac codec not support multi channels */
+		if (pDSP_handle->codec_type == CODEC_AAC_DEC && parameter->channels > 2)
+			return ACODEC_PROFILE_NOT_SUPPORT;
 		param[1] = parameter->channels;
 		break;
 	case UNIA_FRAMED:
